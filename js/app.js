e28bac5b2309e4b3da76defd496a1ab9be6bf098
Jon Hollis <jon@dotcom.co.uk>
2021-06-08 18:10:03 +0100

    ES6 Accordion
    Replaced with arrow functions.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
-function textReveal (){
+// ES5
+// const acc = document.getElementsByClassName('accordion');
 
-  let acc = document.getElementsByClassName('accordion');
+// for (let i = 0; i < acc.length; i++) {
+//   acc[i].addEventListener("click", function() {
 
-  for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+//     let content = this.nextElementSibling;
 
-      let content = this.nextElementSibling;
+    // if(content.style.maxHeight){
+    //   content.style.maxHeight = null;
+    // } else {
+    //   content.style.maxHeight = content.scrollHeight + 'px';
+    // }
+//   });
+// }
 
-      if(content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-      }
-    });
-  }
-}
+// ES 6
+const accordions = document.querySelectorAll('.accordion');
 
-textReveal ();
+accordions.forEach(acc => {
+  acc.addEventListener('click', event => {
+    // hide panel
+    const accOpen = document.querySelector('.accordion.active');
+
+    if (accOpen && accOpen !== acc ){
+      accOpen.classList.toggle('active');
+      accOpen.nextElementSibling.style.maxHeight = 0;
+    }
+
+    // open panel
+    acc.classList.toggle('active');
+    const content = acc.nextElementSibling;
+    if(acc.classList.contains('active')){
+      content.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      content.style.maxHeight = 0;
+    }
+    
+  });
+});
