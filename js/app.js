6a3cdca8610743b8dfbc9830d00c0be15fd29fdb
Jon Hollis <jon@dotcom.co.uk>
2021-06-08 14:05:37 +0100

    Modified CSS
    Removed border radius

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 function textReveal (){
 
-  var acc = document.getElementsByClassName('accordion');
+  let acc = document.getElementsByClassName('accordion');
 
   for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
 
-      var content = this.nextElementSibling;
+      let content = this.nextElementSibling;
 
       if(content.style.maxHeight){
         content.style.maxHeight = null;
